eb381ca8efd81845310bbbf474ed6a3de13964bc
Ali Cetin <ali.cetin@4subsea.com>
2023-08-08 20:59:39 +0200

    docs: configure conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,26 +3,45 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+from datetime import date
+from importlib import metadata
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'SMS Fusion'
-copyright = '2023, 4Subsea'
-author = '4Subsea'
-release = '0.0.1'
+project = "SMS Fusion"
+copyright = f"{date.today().year}, 4Subsea"
+author = "4Subsea"
+release = metadata.version("smsfusion")
+github_repo = "https://github.com/4Subsea/smsfusion-python/"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autosummary",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+]
+autosummary_generate = True
 
-templates_path = ['_templates']
+# Napoleon settings
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+
+# Intershpinx mapping
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+templates_path = ["_templates", "_templates/autosummary"]
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
