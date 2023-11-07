d935db563d2f75547aa084a1281adc0e2970e91b
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-11-07 12:58:40 +0100

    test standard normal seeds

--- tests/test_noise_models.py
+++ tests/test_noise_models.py
@@ -10,3 +10,12 @@ def test__standard_normal():
     assert len(x) == 100
     assert np.mean(x) == pytest.approx(0.0, abs=0.2)  # mean value is zero
     assert np.std(x) == pytest.approx(1.0, abs=1e-1)  # std is 1
+
+
+def test__standard_normal_seeds():
+    x0 = _standard_normal(100, seed=0)
+    x1 = _standard_normal(100, seed=0)
+    x2 = _standard_normal(100, seed=1)
+
+    np.testing.assert_array_almost_equal(x0, x1)
+    assert not np.array_equal(x0, x2)
