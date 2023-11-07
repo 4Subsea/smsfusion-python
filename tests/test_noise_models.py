f0e39f9ae35d5c8df17985b4f15f3a8507a930dc
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-11-07 13:34:55 +0100

    make test more robust

--- tests/test_noise_models.py
+++ tests/test_noise_models.py
@@ -15,7 +15,7 @@ def test__standard_normal():
 
     assert len(x) == 100
     assert np.mean(x) == pytest.approx(0.0, abs=0.2)  # mean value is zero
-    assert np.std(x) == pytest.approx(1.0, abs=1e-1)  # std is 1
+    assert np.std(x) == pytest.approx(1.0, abs=0.2)  # std is 1
 
 
 def test__standard_normal_seeds():
