aa4c2efad2fae0686ae584095dd14203c1d56fa9
Ali Cetin <ali.cetin@4subsea.com>
2023-11-07 09:50:24 +0100

    add tests

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -7,8 +7,8 @@ from smsfusion import benchmark
 class Test_BeatSignal:
     def test_init(self):
         beat_signal = benchmark.BeatSignal(0.1, 0.01)
-        assert beat_signal._f_main == 0.1 * 2. * np.pi
-        assert beat_signal._f_beat == 0.01 * 2. * np.pi
+        assert beat_signal._f_main == 0.1 * 2.0 * np.pi
+        assert beat_signal._f_beat == 0.01 * 2.0 * np.pi
 
     def test_init_rads(self):
         beat_signal = benchmark.BeatSignal(0.1, 0.01, freq_hz=False)
@@ -18,8 +18,8 @@ class Test_BeatSignal:
     def test_signal(self):
         beat_signal = benchmark.BeatSignal(0.1, 0.01, freq_hz=True)
 
-        fs = 10.
-        duration = 200.
+        fs = 10.0
+        duration = 200.0
         t, y, dydt = beat_signal(fs, duration, amp=5.0)
 
         assert len(t) == int(duration * fs)
@@ -28,7 +28,9 @@ class Test_BeatSignal:
 
         assert y.max() == pytest.approx(5.0)
         assert y.min() == pytest.approx(-5.0)
-        assert y.mean() == pytest.approx(0.)
+        assert y.mean() == pytest.approx(0.0)
         assert y.std() == pytest.approx(2.5)
 
-        np.testing.assert_allclose(dydt, np.gradient(y, t), atol=0.0025)  # Verified tolerance
+        np.testing.assert_allclose(
+            dydt, np.gradient(y, t), atol=0.0025
+        )  # Verified tolerance
