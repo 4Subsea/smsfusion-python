8eb71573352c45dcca55b1330047a9631cee2cf9
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-11-13 11:16:05 +0100

    rename noise models file

--- src/smsfusion/noise/__init__.py
+++ src/smsfusion/noise/__init__.py
@@ -1,3 +1,3 @@
-from ._noise_models import gauss_markov, random_walk, white_noise
+from ._noise import gauss_markov, random_walk, white_noise
 
 __all__ = ["gauss_markov", "random_walk", "white_noise"]
