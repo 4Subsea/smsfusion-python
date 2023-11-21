970e4a39265a533472b20b53b2be6952e79dde4b
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-11-21 15:52:35 +0100

    enh: 9dof benchmark signal

--- src/smsfusion/benchmark/__init__.py
+++ src/smsfusion/benchmark/__init__.py
@@ -2,6 +2,7 @@ from ._benchmark import (
     BeatSignal,
     ChirpSignal,
     _benchmark_helper,
+    benchmark_9dof_beat_202311A,
     benchmark_ahrs_beat_202311A,
     benchmark_ahrs_chirp_202311A,
 )
@@ -9,6 +10,7 @@ from ._benchmark import (
 __all__ = [
     "_benchmark_helper",
     "BeatSignal",
+    "benchmark_9dof_beat_202311A",
     "benchmark_ahrs_beat_202311A",
     "benchmark_ahrs_chirp_202311A",
     "ChirpSignal",
