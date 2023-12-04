89f469e146e316466a8bb905105c8f606059331f
Ali Cetin <ali.cetin@4subsea.com>
2023-12-04 23:40:40 +0100

    quat prod

--- src/smsfusion/_vectorops.py
+++ src/smsfusion/_vectorops.py
@@ -23,3 +23,21 @@ def _cross(a: NDArray[np.float64], b: NDArray[np.float64]) -> NDArray[np.float64
             a[0] * b[1] - a[1] * b[0],
         ]
     )
+
+
+@njit  # type: ignore[misc]
+def _quaternion_product(
+    qa: NDArray[np.float64], qb: NDArray[np.float64]
+) -> NDArray[np.float64]:
+    """
+    Unit quaternion (Schur) product.
+    """
+    qa_w, qa_xyz = np.split(qa, [1])
+    qb_w, qb_xyz = np.split(qb, [1])
+    return np.concatenate(
+        (
+            qa_w * qb_w - qa_xyz.T @ qb_xyz,
+            qa_w * qb_xyz + qb_w * qa_xyz + np.cross(qa_xyz, qb_xyz),
+        ),
+        axis=0
+    )
