c24a1a3a20fe23045a9aac2ae0fc4e39a1eac1a3
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-12-05 00:04:02 +0100

    Strapdown algorithm using quaternions (#41)

--- src/smsfusion/_ins.py
+++ src/smsfusion/_ins.py
@@ -7,9 +7,13 @@ from numpy.typing import ArrayLike, NDArray
 from ._ahrs import AHRS
 from ._transforms import (
     _angular_matrix_from_euler,
+    _angular_matrix_from_quaternion,
+    _euler_from_quaternion,
     _quaternion_from_euler,
     _rot_matrix_from_euler,
+    _rot_matrix_from_quaternion,
 )
+from ._vectorops import _normalize
 
 
 def _signed_smallest_angle(angle: float, degrees: bool = True) -> float:
@@ -67,6 +71,224 @@ def gravity(lat: float | None = None, degrees: bool = True) -> float:
 
 
 class StrapdownINS:
+    """
+    Inertial navigation system (INS) strapdown algorithm.
+
+    This class provides an interface for estimating position, velocity and attitude
+    of a moving body by integrating the 'strapdown navigation equations'.
+
+    Parameters
+    ----------
+    x0 : numpy.ndarray (10,)
+        Initial state vector, containing the following elements in order:
+            - Position in x-, y-, and z-direction (3 elements).
+            - Velocity in x-, y-, and z-direction (3 elements).
+            - Attitude as unit quaternion (4 elements). Should be given as
+              [q1, q2, q3, q4], where q1 is the real part and q1, q2 and q3 are
+              the three imaginary parts.
+    lat : float, optional
+        Latitude used to calculate the gravitational acceleration. If `lat` is ``None``,
+        the 'standard gravity' (i.e., 9.80665) is used.
+    """
+
+    def __init__(self, x0: ArrayLike, lat: float | None = None) -> None:
+        self._x0 = np.asarray_chkfinite(x0).reshape(10, 1).copy()
+        self._x = self._x0.copy()
+        self._g = np.array([0, 0, gravity(lat)]).reshape(3, 1)  # gravity vector in NED
+
+    @property
+    def _p(self) -> NDArray[np.float64]:
+        return self._x[0:3]
+
+    @_p.setter
+    def _p(self, p: ArrayLike) -> None:
+        self._x[0:3] = p
+
+    @property
+    def _v(self) -> NDArray[np.float64]:
+        return self._x[3:6]
+
+    @_v.setter
+    def _v(self, v: ArrayLike) -> None:
+        self._x[3:6] = v
+
+    @property
+    def _q(self) -> NDArray[np.float64]:
+        return self._x[6:10]
+
+    @_q.setter
+    def _q(self, q: ArrayLike) -> None:
+        self._x[6:10] = q
+
+    @property
+    def x(self) -> NDArray[np.float64]:
+        """
+        Current state vector estimate.
+
+        Returns
+        -------
+        x : numpy.ndarray (10,)
+            State vector, containing the following elements in order:
+                - Position in x-, y-, and z-direction (3 elements).
+                - Velocity in x-, y-, and z-direction (3 elements).
+                - Attitude as unit quaternion (4 elements). Should be given as
+                  [q1, q2, q3, q4], where q1 is the real part and q1, q2 and q3
+                  are the three imaginary parts.
+        """
+        return self._x.flatten()
+
+    def position(self) -> NDArray[np.float64]:
+        """
+        Current position vector estimate.
+
+        Returns
+        -------
+        p : numpy.ndarray (3,)
+            Position state vector, containing position in x-, y-, and z-direction
+            (in that order).
+        """
+        return self._p.flatten()
+
+    def velocity(self) -> NDArray[np.float64]:
+        """
+        Current velocity vector estimate.
+
+        Returns
+        -------
+        v : numpy.ndarray (3,)
+            Velocity state vector, containing (linear) velocity in x-, y-, and z-direction
+            (in that order).
+        """
+        return self._v.flatten()
+
+    def quaternion(self) -> NDArray[np.float64]:
+        """
+        Current attitude estimate as unit quaternion (from-body-to-NED).
+
+        Returns
+        -------
+        q : numpy.ndarray (4,)
+            Attitude as unit quaternion. Given as [q1, q2, q3, q4], where q1 is
+            the real part and q1, q2 and q3 are the three imaginary parts.
+        """
+        return self._q.flatten()
+
+    def euler(self, degrees: bool = False) -> NDArray[np.float64]:
+        """
+        Current attitude estimate as Euler angles (see Notes).
+
+        Parameters
+        ----------
+        degrees : bool
+            Whether to return the Euler angles in degrees (`True`) or radians (`False`).
+
+        Returns
+        -------
+        euler : numpy.ndarray (3,)
+            Euler angles, specifically: alpha (roll), beta (pitch) and gamma (yaw)
+            in that order.
+
+        Notes
+        -----
+        The Euler angles describe how to transition from the 'NED' frame to the 'body'
+        frame through three consecutive intrinsic and passive rotations in the ZYX order:
+            1. A rotation by an angle gamma (often called yaw) about the z-axis.
+            2. A subsequent rotation by an angle beta (often called pitch) about the y-axis.
+            3. A final rotation by an angle alpha (often called roll) about the x-axis.
+
+        This sequence of rotations is used to describe the orientation of the 'body' frame
+        relative to the 'NED' frame in 3D space.
+
+        Intrinsic rotations mean that the rotations are with respect to the changing
+        coordinate system; as one rotation is applied, the next is about the axis of
+        the newly rotated system.
+
+        Passive rotations mean that the frame itself is rotating, not the object
+        within the frame.
+        """
+        q = self.quaternion()
+        theta = _euler_from_quaternion(q)
+
+        if degrees:
+            theta = (180.0 / np.pi) * theta
+
+        return theta
+
+    def reset(self, x_new: ArrayLike) -> None:
+        """
+        Reset state.
+
+        Parameters
+        ----------
+        x_new : ndarray (10,)
+            New state vector, containing the following elements in order:
+                - Position in x-, y-, and z-direction (3 elements).
+                - Velocity in x-, y-, and z-direction (3 elements).
+                - Attitude as unit quaternion (4 elements). Should be given as
+                  [q1, q2, q3, q4], where q1 is the real part and q1, q2 and q3
+                  are the three imaginary parts.
+        """
+        self._x = np.asarray_chkfinite(x_new).reshape(10, 1).copy()
+
+    def update(
+        self,
+        dt: float,
+        f_ins: ArrayLike,
+        w_ins: ArrayLike,
+        degrees: bool = False,
+    ) -> "StrapdownINS":  # TODO: Replace with ``typing.Self`` when Python > 3.11:
+        """
+        Update the INS states by integrating the 'strapdown navigation equations'.
+
+        Assuming constant inputs (i.e., accelerations and angular velocities) over
+        the sampling period.
+
+        The states are updated according to:
+
+            p[k+1] = p[k] + h * v[k] + 0.5 * dt * a[k]
+
+            v[k+1] = v[k] + dt * a[k]
+
+            q[k+1] = q[k] + dt * T(q[k]) * w_ins[k]
+
+        where,
+
+            a[k] = R(q[k]) * f_ins[k] + g
+
+            g = [0, 0, 9.81]^T
+
+        Parameters
+        ----------
+        dt : float
+            Sampling period in seconds.
+        f_ins : numpy.ndarray (3,)
+            Acceleration / specific force measurements (bias compensated).
+        w_imu : numpy.ndarray (3,)
+            Rotation rate measurements (bias compensated).
+        degrees : bool, default False
+            Whether the rotation rates are given in `degrees` (``True``) or `radians`
+            (``False``).
+        """
+        f_ins = np.asarray_chkfinite(f_ins, dtype=float).reshape(3, 1)
+        w_ins = np.asarray_chkfinite(w_ins, dtype=float).reshape(3, 1)
+
+        R_bn = _rot_matrix_from_quaternion(self.quaternion())  # body-to-ned
+        T = _angular_matrix_from_quaternion(self.quaternion())
+
+        if degrees:
+            w_ins = (np.pi / 180.0) * w_ins
+
+        # State propagation (assuming constant linear acceleration and angular velocity)
+        a = R_bn @ f_ins + self._g
+        self._p = self._p + dt * self._v + 0.5 * dt**2 * a
+        self._v = self._v + dt * a
+        self._q = self._q + dt * T @ w_ins
+        self._q = _normalize(self._q)
+
+        return self
+
+
+class _LegacyStrapdownINS:
     """
     Inertial navigation system (INS) strapdown algorithm.
 
@@ -253,7 +475,9 @@ class StrapdownINS:
         w_imu: ArrayLike,
         degrees: bool = False,
         theta_ext: ArrayLike | None = None,
-    ) -> "StrapdownINS":  # TODO: Replace with ``typing.Self`` when Python > 3.11:
+    ) -> (
+        "_LegacyStrapdownINS"
+    ):  # TODO: Replace with ``typing.Self`` when Python > 3.11:
         """
         Update the INS states by integrating the 'strapdown navigation equations'.
 
@@ -399,7 +623,7 @@ class AidedINS:
 
         # Strapdown algorithm
         self._x_ins = self._x0
-        self._ins = StrapdownINS(self._x_ins[0:9])
+        self._ins = _LegacyStrapdownINS(self._x_ins[0:9])
 
         # Initial Kalman filter error covariance
         self._P_prior = np.eye(15)
--- tests/test_ins.py
+++ tests/test_ins.py
@@ -15,11 +15,20 @@ import pytest
 from pandas import read_parquet
 from scipy.spatial.transform import Rotation
 
-from smsfusion._ins import AHRS, AidedINS, StrapdownINS, _signed_smallest_angle, gravity
+from smsfusion._ins import (
+    AHRS,
+    AidedINS,
+    StrapdownINS,
+    _LegacyStrapdownINS,
+    _signed_smallest_angle,
+    gravity,
+)
 from smsfusion._transforms import (
     _angular_matrix_from_euler,
+    _angular_matrix_from_quaternion,
     _quaternion_from_euler,
     _rot_matrix_from_euler,
+    _rot_matrix_from_quaternion,
 )
 from smsfusion.benchmark import (
     benchmark_full_pva_beat_202311A,
@@ -72,13 +81,186 @@ def ains_ref_data():
 class Test_StrapdownINS:
     @pytest.fixture
     def ins(self):
-        x0 = np.zeros((9, 1))
+        x0 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
         ins = StrapdownINS(x0)
         return ins
 
     def test__init__(self):
-        x0 = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, np.pi, np.pi / 2.0, np.pi / 4.0])
+        x0 = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1.0, 0.0, 0.0, 0.0])
         ins = StrapdownINS(x0)
+
+        np.testing.assert_array_equal(ins._x0, x0.reshape(-1, 1))
+        np.testing.assert_array_equal(ins._x, x0.reshape(-1, 1))
+
+    def test_x(self):
+        x0 = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1.0, 0.0, 0.0, 0.0])
+        ins = StrapdownINS(x0)
+
+        x_out = ins.x
+        x_expect = x0
+
+        assert x_out.shape == (10,)
+        assert x_out is not ins._x
+        np.testing.assert_array_equal(x_out, x_expect)
+
+    def test_position(self):
+        x0 = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1.0, 0.0, 0.0, 0.0])
+        ins = StrapdownINS(x0)
+
+        p_out = ins.position()
+        p_expect = np.array([1.0, 2.0, 3.0])
+
+        assert p_out.shape == (3,)
+        assert p_out is not ins._p
+        np.testing.assert_array_equal(p_out, p_expect)
+
+    def test_velocity(self):
+        x0 = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1.0, 0.0, 0.0, 0.0])
+        ins = StrapdownINS(x0)
+
+        v_out = ins.velocity()
+        v_expect = np.array([4.0, 5.0, 6.0])
+
+        assert v_out.shape == (3,)
+        assert v_out is not ins._v
+        np.testing.assert_array_equal(v_out, v_expect)
+
+    def test_quaternion(self):
+        x0 = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1.0, 0.0, 0.0, 0.0])
+        ins = StrapdownINS(x0)
+
+        q_out = ins.quaternion()
+        q_expect = np.array([1.0, 0.0, 0.0, 0.0])
+
+        assert q_out.shape == (4,)
+        assert q_out is not ins._q
+        np.testing.assert_array_equal(q_out, q_expect)
+
+    def test_euler(self):
+        x0 = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1.0, 0.0, 0.0, 0.0])
+        ins = StrapdownINS(x0)
+
+        theta_out = ins.euler()
+        theta_expect = np.array([0.0, 0.0, 0.0])
+
+        assert theta_out.shape == (3,)
+        np.testing.assert_array_equal(theta_out, theta_expect)
+
+    def test_reset(self, ins):
+        x = np.random.random(10)
+        x[6:10] = x[6:10] / np.linalg.norm(x[6:10])  # unit quaternion
+        ins.reset(x)
+
+        np.testing.assert_array_equal(ins.x, x)
+
+    def test_reset_2d(self, ins):
+        x = np.random.random(10).reshape(-1, 1)
+        x[6:10] = x[6:10] / np.linalg.norm(x[6:10])  # unit quaternion
+        ins.reset(x)
+
+        np.testing.assert_array_equal(ins.x, x.flatten())
+
+    def test_update_return_self(self, ins):
+        dt = 0.1
+        g = 9.80665
+        f = np.array([0.0, 0.0, -g]).reshape(-1, 1)
+        w = np.array([0.0, 0.0, 0.0]).reshape(-1, 1)
+
+        update_return = ins.update(dt, f, w)
+        assert update_return is ins
+
+    def test_update(self, ins):
+        h = 0.1
+        g = ins._g
+        f = np.array([1.0, 2.0, 3.0]).reshape(-1, 1) - g
+        w = np.array([0.04, 0.05, 0.06]).reshape(-1, 1)
+
+        x0_out = ins.x
+        ins.update(h, f, w)
+        x1_out = ins.x
+
+        x0_expect = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
+        x1_expect = np.array(
+            [0.005, 0.01, 0.015, 0.1, 0.2, 0.3, 0.99999, 0.002, 0.0025, 0.003]
+        )
+
+        np.testing.assert_array_almost_equal(x0_out, x0_expect)
+        np.testing.assert_array_almost_equal(x1_out, x1_expect)
+
+    def test_update_deg(self, ins):
+        dt = 0.1
+        g = ins._g
+        f_imu = np.array([1.0, 2.0, 3.0]).reshape(-1, 1) - g
+        w_imu = np.array([4.0, 5.0, 6.0]).reshape(-1, 1)
+
+        x0_out = ins.x
+        ins.update(dt, f_imu, w_imu, degrees=True)
+        x1_out = ins.x
+
+        x0_expect = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0])
+        x1_expect = np.array(
+            [0.005, 0.01, 0.015, 0.1, 0.2, 0.3, 0.999971, 0.003491, 0.004363, 0.005236]
+        )
+
+        np.testing.assert_array_almost_equal(x0_out, x0_expect)
+        np.testing.assert_array_almost_equal(x1_out, x1_expect)
+
+    def test_update_twise(self, ins):
+        dt = 0.1
+        g = ins._g
+        f_imu = np.array([1.0, 2.0, 3.0]).reshape(-1, 1) - g
+        w_imu = np.array([0.004, 0.005, 0.006]).reshape(-1, 1)
+
+        x0_out = ins.x
+        ins.update(dt, f_imu, w_imu, degrees=False)
+        x1_out = ins.x
+        ins.update(dt, f_imu, w_imu, degrees=False)
+        x2_out = ins.x
+
+        x0_expect = np.array(
+            [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+        ).reshape(-1, 1)
+
+        # Calculate x1
+        R0_expect = np.eye(3)
+        T0_expect = _angular_matrix_from_quaternion(x0_expect[6:10].flatten())
+        a0_expect = R0_expect @ f_imu + g
+        x1_expect = np.zeros((10, 1))
+        x1_expect[0:3] = (
+            x0_expect[0:3] + dt * x0_expect[3:6] + 0.5 * dt**2 * a0_expect
+        )
+        x1_expect[3:6] = x0_expect[3:6] + dt * a0_expect
+        x1_expect[6:10] = x0_expect[6:10] + dt * T0_expect @ w_imu
+        x1_expect[6:10] = x1_expect[6:10] / np.linalg.norm(x1_expect[6:10])
+
+        # Calculate x2 by forward Euler
+        R1_expect = _rot_matrix_from_quaternion(x1_expect[6:10].flatten())
+        T1_expect = _angular_matrix_from_quaternion(x1_expect[6:10].flatten())
+        a1_expect = R1_expect @ f_imu + g
+        x2_expect = np.zeros((10, 1))
+        x2_expect[0:3] = (
+            x1_expect[0:3] + dt * x1_expect[3:6] + 0.5 * dt**2 * a1_expect
+        )
+        x2_expect[3:6] = x1_expect[3:6] + dt * a1_expect
+        x2_expect[6:10] = x1_expect[6:10] + dt * T1_expect @ w_imu
+        x2_expect[6:10] = x2_expect[6:10] / np.linalg.norm(x2_expect[6:10])
+
+        np.testing.assert_array_almost_equal(x0_out, x0_expect.flatten())
+        np.testing.assert_array_almost_equal(x1_out, x1_expect.flatten())
+        np.testing.assert_array_almost_equal(x2_out, x2_expect.flatten())
+
+
+@pytest.mark.filterwarnings("ignore")
+class Test_LegacyStrapdownINS:
+    @pytest.fixture
+    def ins(self):
+        x0 = np.zeros((9, 1))
+        ins = _LegacyStrapdownINS(x0)
+        return ins
+
+    def test__init__(self):
+        x0 = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, np.pi, np.pi / 2.0, np.pi / 4.0])
+        ins = _LegacyStrapdownINS(x0)
 
         np.testing.assert_array_equal(ins._x0, x0.reshape(-1, 1))
         np.testing.assert_array_equal(ins._x, x0.reshape(-1, 1))
@@ -159,7 +341,7 @@ class Test_StrapdownINS:
 
     def test_update_return_self(self):
         x0 = np.zeros((9, 1))
-        strapdownins = StrapdownINS(x0)
+        strapdownins = _LegacyStrapdownINS(x0)
 
         dt = 0.1
         g = 9.80665
@@ -171,7 +353,7 @@ class Test_StrapdownINS:
 
     def test_update(self):
         x0 = np.zeros((9, 1))
-        ins = StrapdownINS(x0)
+        ins = _LegacyStrapdownINS(x0)
 
         h = 0.1
         g = ins._g
@@ -190,7 +372,7 @@ class Test_StrapdownINS:
 
     def test_update_deg(self):
         x0 = np.zeros((9, 1))
-        ins = StrapdownINS(x0)
+        ins = _LegacyStrapdownINS(x0)
 
         dt = 0.1
         g = ins._g
@@ -221,7 +403,7 @@ class Test_StrapdownINS:
 
     def test_update_twise(self):
         x0 = np.zeros((9, 1))
-        ins = StrapdownINS(x0)
+        ins = _LegacyStrapdownINS(x0)
 
         dt = 0.1
         g = ins._g
@@ -263,7 +445,7 @@ class Test_StrapdownINS:
         np.testing.assert_array_almost_equal(x2_out, x2_expect.flatten())
 
     def test_update_R_T(self):
-        ins = StrapdownINS(np.zeros((9, 1)))
+        ins = _LegacyStrapdownINS(np.zeros((9, 1)))
 
         h = 0.1
         g = ins._g
@@ -320,7 +502,7 @@ class Test_AidedINS:
         assert ains.ahrs._Kp == 0.050
         assert ains.ahrs._Ki == 0.035
         assert isinstance(ains.ahrs, AHRS)
-        assert isinstance(ains._ins, StrapdownINS)
+        assert isinstance(ains._ins, _LegacyStrapdownINS)
         np.testing.assert_array_almost_equal(ains._x_ins, np.zeros((15, 1)))
         np.testing.assert_array_almost_equal(ains._P_prior, np.eye(15))
 
