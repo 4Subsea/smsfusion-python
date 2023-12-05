76de80fbdd9c1ea6bad8ff0e6ef763fb260cc340
Ali Cetin <ali.cetin@4subsea.com>
2023-12-05 14:28:48 +0100

    enh: add private skew symmetric matrix for cross product operations  (#40)

--- src/smsfusion/_vectorops.py
+++ src/smsfusion/_vectorops.py
@@ -23,3 +23,11 @@ def _cross(a: NDArray[np.float64], b: NDArray[np.float64]) -> NDArray[np.float64
             a[0] * b[1] - a[1] * b[0],
         ]
     )
+
+
+@njit  # type: ignore[misc]
+def _skew_symmetric(a: NDArray[np.float64]) -> NDArray[np.float64]:
+    """
+    Cross product equivalent skew symmetric matrix from a vector.
+    """
+    return np.array([[0.0, -a[2], a[1]], [a[2], 0.0, -a[0]], [-a[1], a[0], 0.0]])
