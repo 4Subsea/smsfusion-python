fe2ad7df5c1cca9ef44cda943f7bd308b305b4df
Ali Cetin <ali.cetin@4subsea.com>
2023-12-05 15:07:17 +0100

    docstring vectorops

--- src/smsfusion/_vectorops.py
+++ src/smsfusion/_vectorops.py
@@ -6,7 +6,17 @@ from numpy.typing import NDArray
 @njit  # type: ignore[misc]
 def _normalize(q: NDArray[np.float64]) -> NDArray[np.float64]:
     """
-    L2 normalization of a vector.
+    L2-normalize a vector.
+
+    Parameters
+    ----------
+    q : numpy.ndarray
+        Vector to be normalized
+
+    Returns
+    -------
+    numpy.ndarray
+        Normalized copy of `q`.
     """
     return q / np.sqrt((q * q).sum())  # type: ignore[no-any-return]  # numpy funcs declare Any as return when given scalar-like
 
@@ -14,7 +24,12 @@ def _normalize(q: NDArray[np.float64]) -> NDArray[np.float64]:
 @njit  # type: ignore[misc]
 def _cross(a: NDArray[np.float64], b: NDArray[np.float64]) -> NDArray[np.float64]:
     """
-    Cross product of two vectors.
+    Calculate the cross product of two vectors.
+
+    Parameters
+    ----------
+    a, b : numpy.ndarray
+        Vector to cross, such that ``a x b``.
     """
     return np.array(
         [
@@ -28,6 +43,12 @@ def _cross(a: NDArray[np.float64], b: NDArray[np.float64]) -> NDArray[np.float64
 @njit  # type: ignore[misc]
 def _skew_symmetric(a: NDArray[np.float64]) -> NDArray[np.float64]:
     """
-    Cross product equivalent skew symmetric matrix from a vector.
+    Construct the cross product equivalent skew symmetric matrix.
+
+    Parameters
+    ----------
+    a : numpy.ndarray
+        Vector in which the skew symmetric matrix is based on, such that
+        ``a x b = S(a) b``.
     """
     return np.array([[0.0, -a[2], a[1]], [a[2], 0.0, -a[0]], [-a[1], a[0], 0.0]])
