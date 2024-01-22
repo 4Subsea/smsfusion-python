5f22d03f0c5f360964f7b3d40c1ce0ab05107055
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2024-01-22 14:57:40 +0100

    cleanup

--- src/smsfusion/__init__.py
+++ src/smsfusion/__init__.py
@@ -1,11 +1,10 @@
 from . import benchmark, noise
 from ._ahrs import AHRS
-from ._ins import AidedINS, StrapdownINS, gravity
+from ._ins import StrapdownINS, gravity
 from ._mekf import MEKF
 
 __all__ = [
     "AHRS",
-    "AidedINS",
     "benchmark",
     "gravity",
     "MEKF",
