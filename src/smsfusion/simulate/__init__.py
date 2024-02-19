ecb852252ed09b42d730eaf7d84bd34a99df53ec
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2024-02-19 18:43:17 +0100

    rename to PCASignal

--- src/smsfusion/simulate/__init__.py
+++ src/smsfusion/simulate/__init__.py
@@ -1,4 +1,4 @@
-from ._sim_ins import IMUSignal202402A
+from ._sim_ins import PVASignal202402A
 from ._simulate import GaussMarkov, MonteCarlo, RandomWalk
 
-__all__ = ["MonteCarlo", "GaussMarkov", "RandomWalk", "IMUSignal202402A"]
+__all__ = ["MonteCarlo", "GaussMarkov", "RandomWalk", "PVASignal202402A"]
