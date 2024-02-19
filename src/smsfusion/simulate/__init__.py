da9ebcf031121c14b102dd1335f1081ca3e0864b
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2024-02-19 13:22:51 +0100

    gauss markov 1st

--- src/smsfusion/simulate/__init__.py
+++ src/smsfusion/simulate/__init__.py
@@ -1,3 +1,3 @@
-from ._montecarlo import MonteCarlo, random_walk
+from ._montecarlo import MonteCarlo, gauss_markov_1st, random_walk
 
-__all__ = ["MonteCarlo", "random_walk"]
+__all__ = ["MonteCarlo", "random_walk", "gauss_markov_1st"]
