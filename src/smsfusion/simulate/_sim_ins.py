23f173a88b06d75b1f2a056c6242f0baf2037fbc
vegard-solum-4ss <vegard.solum@4subsea.com>
2024-02-20 09:49:45 +0100

    some changes

--- src/smsfusion/simulate/_sim_ins.py
+++ src/smsfusion/simulate/_sim_ins.py
@@ -4,6 +4,62 @@ from .._ins import gravity
 from .._transforms import _angular_matrix_from_quaternion, _rot_matrix_from_quaternion
 
 
+def pva_signal_202402A(fs, n):
+    dt = 1.0 / fs
+    t = np.arange(0.0, n * dt, dt)
+
+    g_ned = np.array([0.0, 0.0, gravity()])
+
+    # Alias for transformation nmatrices
+    R_bn = _rot_matrix_from_quaternion  # rotation matrix body-to-ned
+    T = _angular_matrix_from_quaternion  # angular rates to quaternion rates
+
+    # Linear acceleration signal (expressed in body frame)
+    acc = np.array(
+        [
+            0.1 * np.sin(0.1 * t),
+            0.1 * np.cos(0.1 * t),
+            0.05 * np.sin(0.05 * t),
+        ]
+    )
+
+    # Angular rate signal (expressed in body frame)
+    ang_rate = np.array(
+        [
+            0.01 * np.cos(0.2 * t),
+            -0.02 * np.sin(0.1 * t),
+            0.01 * np.sin(0.1 * t),
+        ]
+    )
+
+    # Simulate
+    x0 = np.zeros(10)
+    x0[6:10] = [1.0, 0.0, 0.0, 0.0]
+    x_pred = x0
+    x, f, w = [], [], []
+    for k, (a_k, w_k) in enumerate(zip(acc.T, ang_rate.T)):
+
+        # Update current state
+        x_k = x_pred
+        v_k = x_k[3:6]
+        q_k = x_k[6:10]
+        x.append(x_k)
+
+        # Specific force
+        f_k = a_k - R_bn(x_k[6:10]).T @ g_ned
+        f.append(f_k)
+
+        # Propagate state vector
+        x_dot_k = np.r_[v_k, R_bn(q_k) @ a_k, T(q_k) @ w_k]
+        x_pred = x_k + x_dot_k * dt
+
+    x = np.asarray(x)
+    f = np.asarray(f)
+    w = ang_rate
+
+    return x, f, w
+
+
 class PVASignal202402A:
     """
     Position, velocity and attitude (PVA) and IMU signals (without noise) as implemented
