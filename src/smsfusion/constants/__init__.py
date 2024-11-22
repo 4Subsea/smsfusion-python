fbda6e44870251041506629659fd11c02f90ce32
Vegard R. Solum <vegard.solum@4subsea.com>
2024-11-22 11:04:56 +0100

    motion2

--- src/smsfusion/constants/__init__.py
+++ src/smsfusion/constants/__init__.py
@@ -1,3 +1,8 @@
-from ._constants import ERR_ACC_MOTION1, ERR_GYRO_MOTION1
+from ._constants import (
+    ERR_ACC_MOTION1,
+    ERR_GYRO_MOTION1,
+    ERR_ACC_MOTION2,
+    ERR_GYRO_MOTION2,
+)
 
-__all__ = ["ERR_ACC_MOTION1", "ERR_GYRO_MOTION1"]
+__all__ = ["ERR_ACC_MOTION1", "ERR_ACC_MOTION2", "ERR_GYRO_MOTION1", "ERR_GYRO_MOTION2"]
--- src/smsfusion/constants/_constants.py
+++ src/smsfusion/constants/_constants.py
@@ -9,3 +9,15 @@ ERR_GYRO_MOTION1 = {
     "B": 0.00004,
     "tau_cb": 50,
 }
+
+ERR_ACC_MOTION2 = {
+    "N": 0.0005,
+    "B": 0.0003,
+    "tau_cb": 50.0,
+}
+
+ERR_GYRO_MOTION2 = {
+    "N": 0.00004,
+    "B": 0.00002,
+    "tau_cb": 50,
+}
