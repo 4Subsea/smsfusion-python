f271e0dbcaa13021405270e9a5979e2319dec3ee
Ali Cetin <ali.cetin@4subsea.com>
2024-11-25 15:18:30 +0100

    fix tests

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -5,9 +5,9 @@ from smsfusion import constants
 
 def test_ERR_ACC_MOTION1():
     err_expect = {
-        "N": 0.003,
-        "B": 0.0008,
-        "tau_cb": 100.0,
+        "N": 0.004,
+        "B": 0.0007,
+        "tau_cb": 50.0,
     }
 
     err_out = constants.ERR_ACC_MOTION1
@@ -20,9 +20,9 @@ def test_ERR_ACC_MOTION1():
 
 def test_ERR_GYRO_MOTION1():
     err_expect = {
-        "N": 0.00007,
-        "B": 0.00004,
-        "tau_cb": 50,
+        "N": 0.00009,
+        "B": 0.00003,
+        "tau_cb": 50.0,
     }
 
     err_out = constants.ERR_GYRO_MOTION1
@@ -35,8 +35,8 @@ def test_ERR_GYRO_MOTION1():
 
 def test_ERR_ACC_MOTION2():
     err_expect = {
-        "N": 0.0005,
-        "B": 0.0003,
+        "N": 0.0007,
+        "B": 0.0005,
         "tau_cb": 50.0,
     }
 
@@ -50,8 +50,8 @@ def test_ERR_ACC_MOTION2():
 
 def test_ERR_GYRO_MOTION2():
     err_expect = {
-        "N": 0.00004,
-        "B": 0.00002,
+        "N": 0.00005,
+        "B": 0.00003,
         "tau_cb": 50,
     }
 
