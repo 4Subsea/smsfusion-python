f8260ae5058213c5d4aa0b32d94c000e3a485780
Vegard R. Solum <vegard.solum@4subsea.com>
2024-12-05 13:09:07 +0100

    constants units

--- src/smsfusion/constants/_constants.py
+++ src/smsfusion/constants/_constants.py
@@ -1,23 +1,23 @@
 ERR_ACC_MOTION1 = {
-    "N": 0.004,
-    "B": 0.0007,
-    "tau_cb": 50.0,
+    "N": 0.004,  # (m/s^2)/sqrt(Hz)
+    "B": 0.0007,  # m/s^2
+    "tau_cb": 50.0,  # s
 }
 
 ERR_GYRO_MOTION1 = {
-    "N": 0.00009,
-    "B": 0.00003,
-    "tau_cb": 50.0,
+    "N": 0.00009,  # (rad/s)/sqrt(Hz)
+    "B": 0.00003,  # rad/s
+    "tau_cb": 50.0,  # s
 }
 
 ERR_ACC_MOTION2 = {
-    "N": 0.0007,
-    "B": 0.0005,
-    "tau_cb": 50.0,
+    "N": 0.0007,  # (m/s^2)/sqrt(Hz)
+    "B": 0.0005,  # m/s^2
+    "tau_cb": 50.0,  # s
 }
 
 ERR_GYRO_MOTION2 = {
-    "N": 0.00005,
-    "B": 0.00003,
-    "tau_cb": 50.0,
+    "N": 0.00005,  # (rad/s)/sqrt(Hz)
+    "B": 0.00003,  # rad/s
+    "tau_cb": 50.0,  # s
 }
