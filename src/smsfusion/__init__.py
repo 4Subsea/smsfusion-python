6251f3ebd7c33bcb7bd9b2fd23fcff1a606f7a49
Vegard R. Solum <vegard.solum@4subsea.com>
2025-04-24 08:44:10 +0200

    delete old AHRS

--- src/smsfusion/__init__.py
+++ src/smsfusion/__init__.py
@@ -1,9 +1,7 @@
 from . import benchmark, calibrate, constants, noise
-from ._ahrs import AHRS
 from ._ins import VRU, AidedINS, FixedNED, StrapdownINS, gravity
 
 __all__ = [
-    "AHRS",
     "AidedINS",
     "benchmark",
     "constants",
