ab1a4979603706dadb7a38cfeea9c8d578333c2d
Vegard R. Solum <vegard.solum@4subsea.com>
2025-05-16 10:17:07 +0200

    delete old backward_sweep function

--- src/smsfusion/__init__.py
+++ src/smsfusion/__init__.py
@@ -1,11 +1,10 @@
 from . import benchmark, calibrate, constants, noise
 from ._ins import AHRS, VRU, AidedINS, FixedNED, StrapdownINS, gravity
-from ._smoothing import FixedIntervalSmoother, backward_sweep
+from ._smoothing import FixedIntervalSmoother
 
 __all__ = [
     "AHRS",
     "AidedINS",
-    "backward_sweep",
     "benchmark",
     "constants",
     "calibrate",
