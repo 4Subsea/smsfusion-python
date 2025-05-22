fb97176bc26588425a916b1b74d1a591862fe866
Ali Cetin <ali.cetin@4subsea.com>
2025-05-22 14:03:32 +0200

    style

--- src/smsfusion/constants/_constants.py
+++ src/smsfusion/constants/_constants.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-
 P0 = np.eye(12) * 1e-6
 
 # Noise and bias parameters for SMS Motion Gen 2
 ERR_ACC_MOTION2 = {
-    "N": 0.0007, # (m/s^2)/sqrt(Hz)
+    "N": 0.0007,  # (m/s^2)/sqrt(Hz)
     "B": 0.0005,  # m/s^2
     "tau_cb": 50.0,  # s
 }
