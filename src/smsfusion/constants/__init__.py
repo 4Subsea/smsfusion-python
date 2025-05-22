41f7bf59ecd23ec9125ce147e36a7fd555af741d
Ali Cetin <ali.cetin@4subsea.com>
2025-05-22 16:59:09 +0200

    enh: update default values for SMS Motion Gen 2 noise parameters to latest official (#87)

--- src/smsfusion/constants/__init__.py
+++ src/smsfusion/constants/__init__.py
@@ -1,15 +1,11 @@
 from ._constants import (
-    ERR_ACC_MOTION1,
     ERR_ACC_MOTION2,
-    ERR_GYRO_MOTION1,
     ERR_GYRO_MOTION2,
     P0,
 )
 
 __all__ = [
-    "ERR_ACC_MOTION1",
     "ERR_ACC_MOTION2",
-    "ERR_GYRO_MOTION1",
     "ERR_GYRO_MOTION2",
     "P0",
 ]
