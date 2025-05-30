916cf2ea5ff72183ebc8ae64dd366a5d0ba223c2
Vegard R. Solum <vegard.solum@4subsea.com>
2025-05-30 08:57:07 +0200

    test smoother init

---
+++ tests/test_smoothing.py
@@ -0,0 +1,19 @@
+import pytest
+
+import numpy as np
+import smsfusion as sf
+from smsfusion import FixedIntervalSmoother
+
+
+class Test_FixedIntervalSmoother:
+    @pytest.fixture
+    def ains(self):
+        x0 = np.zeros(16)
+        x0[6:10] = np.array([1.0, 0.0, 0.0, 0.0])
+        ains = sf.AidedINS(10.24, x0)
+        return ains
+    
+    def test__init__(self, ains):
+        smoother = FixedIntervalSmoother(ains)
+        assert smoother._ains is ains
+        assert smoother._cov_smoothing is True
