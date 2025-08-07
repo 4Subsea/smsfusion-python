2a2d049152642a537fefe5a1ec0b8e5946ba2480
Vegard R. Solum <vegard.solum@4subsea.com>
2025-08-07 13:38:08 +0200

    default warmup 10 sec

--- src/smsfusion/_ins.py
+++ src/smsfusion/_ins.py
@@ -667,10 +667,10 @@ class AidedINS(INSMixin):
         calibration. A cold start, on the other hand, performs an initial calibration
         (or warmup) before initializing the filter. This calibration phase refines
         the initial state estimate to reduce the risk of divergence. By default,
-        the calibration period is set to 60 seconds, but it can be adjusted via
+        the calibration period is set to 10 seconds, but it can be adjusted via
         the ``warmup_period`` parameter. The IMU should remain stationary during
         the calibration period.
-    warmup_period : float, default 60.0
+    warmup_period : float, default 10.0
         Duration of the calibration period in seconds. Only relevant for 'cold' starts.
         The IMU should be stationary during this period.
     warmup_smoothing_factor : float, default 0.8
@@ -708,7 +708,7 @@ class AidedINS(INSMixin):
         lever_arm: ArrayLike = np.zeros(3),
         ignore_bias_acc: bool = True,
         warm: bool = False,
-        warmup_period: float = 60.0,
+        warmup_period: float = 10.0,
         warmup_smoothing_factor: float = 0.8,
     ) -> None:
         self._fs = fs
