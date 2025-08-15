a2109a85e6c92b70bbe46d8f19870d64b59ff1d4
Vegard R. Solum <vegard.solum@4subsea.com>
2025-08-15 14:32:25 +0200

    delete obsolete _standard_normal function

--- tests/test_noise.py
+++ tests/test_noise.py
@@ -14,26 +14,8 @@ from smsfusion.noise import (
     random_walk,
     white_noise,
 )
-from smsfusion.noise._noise import _standard_normal
 
 TEST_PATH = Path(__file__).parent
-
-
-def test__standard_normal():
-    x = _standard_normal(100)
-
-    assert len(x) == 100
-    assert np.mean(x) == pytest.approx(0.0, abs=0.3)  # mean value is zero
-    assert np.std(x) == pytest.approx(1.0, abs=0.2)  # std is 1
-
-
-def test__standard_normal_seeds():
-    x0 = _standard_normal(100, seed=0)
-    x1 = _standard_normal(100, seed=0)
-    x2 = _standard_normal(100, seed=1)
-
-    np.testing.assert_array_almost_equal(x0, x1)
-    assert not np.array_equal(x0, x2)
 
 
 def test_white_noise():
