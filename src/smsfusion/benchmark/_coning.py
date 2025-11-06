d9c426910d838faf27f16242bcaece34d915a692
Vegard R. Solum <vegard.solum@4subsea.com>
2025-11-06 17:25:49 +0100

    docstring

---
+++ src/smsfusion/benchmark/_coning.py
@@ -0,0 +1,15 @@
+class ConingTrajectorySimulator:
+    """
+    Coning trajectory generator and IMU simulator.
+
+    A coning trajectory is defined as a circular motion of a vector, r, of constant
+    amplitude, making a constant angle, theta, with respect to a fixed axis. Here,
+    the fixed axis is the z-axis.
+    """
+
+
+    def __init__(self):
+        pass
+
+    def __call__(self, n: int):
+        pass
