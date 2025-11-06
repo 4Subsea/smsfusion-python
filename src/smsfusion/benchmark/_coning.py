f0bf3172892fac5f277c3f3ebb6accb1cfca923a
Vegard R. Solum <vegard.solum@4subsea.com>
2025-11-06 17:30:26 +0100

    docstring

--- src/smsfusion/benchmark/_coning.py
+++ src/smsfusion/benchmark/_coning.py
@@ -5,6 +5,17 @@ class ConingTrajectorySimulator:
     A coning trajectory is defined as a circular motion of a vector, r, of constant
     amplitude, making a constant angle, theta, with respect to a fixed axis. Here,
     the fixed axis is the z-axis.
+
+    Let,
+    - R be the amplitude of the vector, r.
+    - theta be the coning (half) angle. I.e., the angle between r and the z-axis.
+    - phi be the heading angle. I.e., the angle between the projection of r onto the
+      x-y plane and the x-axis.
+    
+    Then, the vector, r, can be expressed as:
+
+        r(t) = R * [sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta)]^T
+
     """
 
 
