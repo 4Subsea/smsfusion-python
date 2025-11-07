6db8baf50ea08f62f276137d03fe771483580362
Vegard R. Solum <vegard.solum@4subsea.com>
2025-11-07 16:43:07 +0100

    delete old code

--- src/smsfusion/benchmark/_coning.py
+++ src/smsfusion/benchmark/_coning.py
@@ -1,45 +1,5 @@
 import numpy as np
 
-# class ConingTrajectorySimulator:
-#     """
-#     Coning trajectory generator and IMU simulator.
-
-#     A coning trajectory is defined as a circular motion of a vector, r, of constant
-#     amplitude, making a constant angle, theta, with respect to a fixed axis. Here,
-#     the fixed axis is the z-axis.
-
-#     Let,
-#     - R be the amplitude of the vector, r.
-#     - theta be the coning (half) angle. I.e., the angle between r and the z-axis.
-#     - phi be the heading angle. I.e., the angle between the projection of r onto the
-#       x-y plane and the x-axis.
-
-#     Then, the vector, r, can be expressed as:
-
-#         r(t) = R * [sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta)]^T
-
-#     """
-
-
-#     def __init__(self):
-#         pass
-
-#     def __call__(self, n: int):
-#         pass
-
-
-# class ConingTrajectorySimulator:
-#     """
-#     Coning trajectory generator and IMU simulator.
-#     """
-
-
-#     def __init__(self):
-#         pass
-
-#     def __call__(self, n: int):
-#         pass
-
 
 class ConingSimulator:
     """
