e9116eca7e495afec53c0ca6a38da78953a189ba
Vegard R. Solum <vegard.solum@4subsea.com>
2025-12-08 13:35:12 +0100

    some changesw

--- src/smsfusion/_coning_sculling.py
+++ src/smsfusion/_coning_sculling.py
@@ -53,6 +53,82 @@ class ConingScullingAlg:
         self._u = np.zeros(3, dtype=float)
         self._f_prev = None
 
+    # def _coning_update(self, w):
+    #     """
+    #     Update the coning integrals using new gyroscope measurements, w[l+1].
+
+    #     Coning algorithm:
+
+    #         phi := beta + dbeta
+
+    #     where,
+
+    #         dtheta[l] = dt * (w[l+1] - w[l])
+    #         dbeta[l+1] = dbeta[l] + 0.5 * cross((beta[l] + (1/6) * dtheta[l-1]), dtheta[l])
+    #         beta[l+1] = beta[l] + dtheta[l]
+
+    #     with initial conditions,
+    #         beta[0] = [0, 0, 0]
+    #         dbeta[0] = [0, 0, 0]
+
+    #     Parameters
+    #     ----------
+    #     w : numpy.ndarray
+    #         Bias corrected angular rate measurements.
+    #     """
+    #     # if self._w_prev is None:  # first sample
+    #     #     dtheta = w * self._dt  # backward Euler
+    #     # else:
+    #     #     dtheta = 0.5 * (w + self._w_prev) * self._dt  # trapezoidal
+    #     dtheta = w * self._dt  # backward Euler
+    #     self._dbeta += 0.5 * _cross(
+    #         self._beta + (1.0 / 6.0) * self._dtheta_prev, dtheta
+    #     )
+    #     self._beta += dtheta
+
+    #     self._w_prev = w
+    #     self._dtheta_prev = dtheta
+
+    # def _sculling_update(self, f):
+    #     """
+    #     Update the sculling integrals using new accelerometer measurements, f[l+1].
+    #     """
+    #     # dvel = f * self._dt
+    #     # if self._f_prev is None:  # first sample
+    #     #     dvel = f * self._dt  # backward Euler
+    #     # else:
+    #     #     dvel = 0.5 * (f + self._f_prev) * self._dt  # trapezoidal
+    #     dvel = f * self._dt  # backward Euler
+    #     self._gamma1 += np.cross(self._beta + 0.5 * self._dtheta_prev, dvel)
+    #     self._u += dvel
+    #     self._f_prev = f
+
+    # def update(self, f_imu: ArrayLike, w_imu: ArrayLike, degrees: bool = False):
+    #     """
+    #     Update.
+
+    #     Parameters
+    #     ----------
+    #     f_imu : array-like, shape (3,)
+    #         Specific force measurements (i.e., accelerations + gravity), given
+    #         as [f_x, f_y, f_z]^T where f_x, f_y and f_z are
+    #         acceleration measurements in x-, y-, and z-direction, respectively.
+    #     w_imu : array-like, shape (3,)
+    #         Angular rate measurements, given as [w_x, w_y, w_z]^T where
+    #         w_x, w_y and w_z are angular rates about the x-, y-,
+    #         and z-axis, respectively.
+    #     degrees : bool, default False
+    #         Specify whether the angular rates are given in degrees or radians.
+    #     """
+    #     f_imu = np.asarray(f_imu, dtype=float)
+    #     w_imu = np.asarray(w_imu, dtype=float)
+
+    #     if degrees:
+    #         w_imu = (np.pi / 180.0) * w_imu
+
+    #     self._sculling_update(f_imu)
+    #     self._coning_update(w_imu)
+
     def update(self, f: ArrayLike, w: ArrayLike, degrees: bool = False):
         """
         Update the coning (dtheta) and sculling (dvel) integrals using new IMU measurements.
@@ -76,12 +152,15 @@ class ConingScullingAlg:
             w = (np.pi / 180.0) * w
 
         # Accelerometer and gyro pulse vector counts from l to l+1
-        if self._f_prev is None or self._w_prev is None:  # first sample
-            dvel = f * self._dt  # backward Euler
-            dtheta = w * self._dt  # backward Euler
-        else:
-            dvel = 0.5 * (f + self._f_prev) * self._dt  # trapezoidal
-            dtheta = 0.5 * (w + self._w_prev) * self._dt  # trapezoidal
+        # if self._f_prev is None or self._w_prev is None:  # first sample
+        #     dvel = f * self._dt  # backward Euler
+        #     dtheta = w * self._dt  # backward Euler
+        # else:
+        #     dvel = 0.5 * (f + self._f_prev) * self._dt  # trapezoidal
+        #     dtheta = 0.5 * (w + self._w_prev) * self._dt  # trapezoidal
+
+        dvel = f * self._dt  # backward Euler
+        dtheta = w * self._dt  # backward Euler
 
         # Sculling update
         self._gamma1 += np.cross(self._beta + 0.5 * dtheta, dvel)
