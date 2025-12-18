491aa06b8a4eb752bcbf2f70af1cda28b77442f8
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2025-12-18 15:46:30 +0100

    feat: coning and sculling algorithm (#100)

---
+++ src/smsfusion/_coning_sculling.py
@@ -0,0 +1,162 @@
+import numpy as np
+from numpy.typing import ArrayLike
+
+from smsfusion._vectorops import _cross
+
+
+class ConingScullingAlg:
+    """
+    Coning and sculling algorithm.
+
+    Integrates the specific force and angular rate measurements to coning and
+    sculling corrected velocity (dvel) and attitude (dtheta) changes.
+
+    Can be used in a strapdown algorithm as:
+
+        vel[m+1] = vel[m] + R(q[m]) @ dvel[m] + dvel_corr
+        q[m+1] = q[m] ⊗ dq(dtheta[m])
+
+    where,
+
+        dvel_corr = [0, 0, g * dt] (if 'NED')
+        dvel_corr = [0, 0, -g * dt] (if 'ENU')
+
+    and,
+
+    - dvel[m] is the sculling integral, i.e., the velocity vector change (no gravity
+      correction) from time step m to m+1.
+    - dtheta[m] is the coning integral, i.e., the rotation vector change from time
+      step m to m+1.
+    - dq(dtheta[m]) is the unit quaternion representation of the rotation increment
+      over the interval [m, m+1].
+    - R(q[m]) is the rotation matrix (body-to-nav) corresponding to the attitude
+      quaternion q[m].
+
+    Here, ⊗ denotes quaternion multiplication (Hamilton product).
+
+    The coning and sculling integrals are computed using a 2nd order algorithm as
+    described in [1]_ and [2]_.
+
+    References
+    ----------
+    .. [1] Savage, Paul G., Strapdown System Algorithms, AD-P003 621, https://apps.dtic.mil/sti/tr/pdf/ADP003621.pdf
+    .. [2] Savage, Paul G., Strapdown Analytics, 2nd Edition, Part 1, 2007, https://strapdownassociates.com/Strapdown%20Analytics%20II%20Part%201.pdf
+    """
+
+    def __init__(self, fs: float):
+        self._fs = fs
+        self._dt = 1.0 / fs
+
+        # Coning params
+        self._theta = np.zeros(3, dtype=float)
+        self._dtheta_con = np.zeros(3, dtype=float)
+        self._dtheta_prev = np.zeros(3, dtype=float)
+
+        # Sculling params
+        self._vel = np.zeros(3, dtype=float)
+        self._dvel_scul = np.zeros(3, dtype=float)
+        self._dv_prev = np.zeros(3, dtype=float)
+
+    def update(self, f: ArrayLike, w: ArrayLike, degrees: bool = False):
+        """
+        Update the coning (dtheta) and sculling (dvel) integrals using new measurements.
+
+        Parameters
+        ----------
+        f : array-like, shape (3,)
+            Specific force (acceleration + gravity) measurements [f_x, f_y, f_z],
+            where f_x, f_y and f_z are specific forces along the x-, y-, and z-axis,
+            respectively.
+        w : array-like, shape (3,)
+            Angular rate measurements [w_x, w_y, w_z], where w_x, w_y and w_z are
+            angular rates about the x-, y-, and z-axis, respectively.
+        degrees : bool, default False
+            Specifies whether the angular rates are given in degrees or radians (default).
+        """
+        f = np.asarray(f, dtype=float)
+        w = np.asarray(w, dtype=float)
+
+        if degrees:
+            w = (np.pi / 180.0) * w
+
+        # View for readability
+        theta = self._theta
+        dtheta_con = self._dtheta_con
+        dtheta_prev = self._dtheta_prev
+        vel = self._vel
+        dvel_scul = self._dvel_scul
+        dv_prev = self._dv_prev
+
+        dv = f * self._dt  # backward Euler
+        dtheta = w * self._dt  # backward Euler
+
+        # Sculling update (2nd order)
+        # See Eq. (7.2.2.2.2-15) in ref [2]_ and Eq. (56) in ref [1]_
+        dvel_scul += 0.5 * (
+            _cross(theta + (1.0 / 6.0) * dtheta_prev, dv)
+            + _cross(vel + (1.0 / 6.0) * dv_prev, dtheta)
+        )
+        vel += dv
+
+        # Coning update
+        # See Eq. (26) in ref [1]_
+        dtheta_con += 0.5 * _cross(theta + (1.0 / 6.0) * dtheta_prev, dtheta)
+        theta += dtheta
+
+        dv_prev[:] = dv
+        dtheta_prev[:] = dtheta
+
+    def dtheta(self, degrees=False):
+        """
+        Peek at the accumulated 'body attitude change' vector. I.e., the rotation
+        vector describing the total rotation over all samples since initialization
+        (or last reset).
+
+        Parameters
+        ----------
+        degrees : bool, default False
+            Specifies whether the returned rotation vector should be in degrees
+            or radians (default).
+        """
+        dtheta = self._theta + self._dtheta_con
+        return np.degrees(dtheta) if degrees else dtheta
+
+    @property
+    def _dvel_rot(self):
+        return 0.5 * _cross(self._theta, self._vel)
+
+    def dvel(self):
+        """
+        Peek at the accumulated specific force velocity change vector. I.e.,
+        the total change in velocity (no gravity correction) over all samples since
+        initialization (or last reset).
+        """
+        return self._vel + self._dvel_rot + self._dvel_scul
+
+    def flush(self, degrees=False):
+        """
+        Return dtheta (the accumulated 'body attitude change' vector) and
+        dvel (the accumulated specific force velocity change vector), and reset
+        the coning (dtheta) and sculling (dvel) integrals to zero.
+
+        Parameters
+        ----------
+        degrees : bool, default False
+            Specifies whether the returned rotation vector should be in degrees
+            or radians (default).
+
+        Returns
+        -------
+        dtheta : ndarray, shape (3,)
+            The accumulated 'body attitude change' vector, see :meth:`dtheta`.
+        dvel : ndarray, shape (3,)
+            The accumulated specific force velocity change vector, see :meth:`dvel`.
+        """
+        dtheta = self.dtheta(degrees=degrees)
+        dvel = self.dvel()
+
+        self._theta[:] = np.zeros(3, dtype=float)
+        self._dtheta_con[:] = np.zeros(3, dtype=float)
+        self._dvel_scul[:] = np.zeros(3, dtype=float)
+        self._vel[:] = np.zeros(3, dtype=float)
+        return dtheta, dvel
---
+++ tests/test_coning_sculling.py
@@ -0,0 +1,335 @@
+from pathlib import Path
+from turtle import down
+
+import numpy as np
+import pytest
+
+import smsfusion as sf
+
+TEST_PATH = Path(__file__).parent
+
+
+@pytest.fixture
+def data_ag():
+    """
+    200 Hz AG data.
+    """
+
+    data = np.genfromtxt(
+        TEST_PATH
+        / "testdata/coning_sculling/coning_sculling_sim_highfreq_20251218A.csv",
+        delimiter=",",
+        names=True,
+        dtype=float,
+    )
+
+    gx = data["Gx_rads"]
+    gy = data["Gy_rads"]
+    gz = data["Gz_rads"]
+    ax = data["Ax_ms2"]
+    ay = data["Ay_ms2"]
+    az = data["Az_ms2"]
+
+    w = np.column_stack((gx, gy, gz))
+    f = np.column_stack((ax, ay, az))
+
+    return f, w
+
+
+@pytest.fixture
+def data_dtheta_dvel():
+    """
+    10 Hz coning/sculling reference data.
+    """
+
+    data = np.genfromtxt(
+        TEST_PATH
+        / "testdata/coning_sculling/coning_sculling_sim_lowfreq_20251218A.csv",
+        delimiter=",",
+        names=True,
+        dtype=float,
+    )
+
+    dtheta_x = data["dThetaX_rad"]
+    dtheta_y = data["dThetaY_rad"]
+    dtheta_z = data["dThetaZ_rad"]
+    dvel_x = data["dVelX_ms"]
+    dvel_y = data["dVelY_ms"]
+    dvel_z = data["dVelZ_ms"]
+
+    dtheta = np.column_stack((dtheta_x, dtheta_y, dtheta_z))
+    dvel = np.column_stack((dvel_x, dvel_y, dvel_z))
+
+    return dvel, dtheta
+
+
+class Test_ConingScullingAlg:
+
+    def test__init__(self):
+        alg = sf.ConingScullingAlg(256.0)
+
+        alg._fs == 256.0
+        alg._dt == 1.0 / 256.0
+        np.testing.assert_allclose(alg._theta, np.zeros(3))
+        np.testing.assert_allclose(alg._dtheta_con, np.zeros(3))
+        np.testing.assert_allclose(alg._dtheta_prev, np.zeros(3))
+        np.testing.assert_allclose(alg._vel, np.zeros(3))
+        np.testing.assert_allclose(alg._dvel_scul, np.zeros(3))
+        np.testing.assert_allclose(alg._dv_prev, np.zeros(3))
+
+    def test_update(self, data_ag, data_dtheta_dvel):
+        f, w = data_ag
+        dvel_ref, dtheta_ref = data_dtheta_dvel
+
+        fs_highfreq = 200.0
+        fs_lowfreq = 10.0
+        step = int(fs_highfreq / fs_lowfreq)
+        alg = sf.ConingScullingAlg(200.0)
+
+        dtheta_out = []
+        dvel_out = []
+        for i, (w_i, f_i) in enumerate(zip(w, f)):
+
+            alg.update(f_i, w_i)
+
+            if (i != 0) and (i % step == 0.0):
+                dtheta_i, dvel_i = alg.flush()
+                dtheta_out.append(dtheta_i)
+                dvel_out.append(dvel_i)
+
+        dtheta_out = np.array(dtheta_out)
+        dvel_out = np.array(dvel_out)
+
+        np.testing.assert_allclose(dvel_out, dvel_ref, atol=1e-8)
+        np.testing.assert_allclose(dtheta_out, dtheta_ref, atol=1e-8)
+
+    def test_update_pure_roll(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([0.0, 0.0, 0.0])  # m/s^2
+        w = np.array([np.radians(90.0), 0.0, 0.0])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out, dvel_out = alg.flush()
+
+        dtheta_expect = np.array([np.radians(90.0), 0.0, 0.0])
+        np.testing.assert_allclose(dtheta_out, dtheta_expect)
+        np.testing.assert_allclose(dvel_out, np.zeros(3))
+
+    def test_update_pure_pitch(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([0.0, 0.0, 0.0])  # m/s^2
+        w = np.array([0.0, np.radians(90.0), 0.0])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out, dvel_out = alg.flush()
+
+        dtheta_expect = np.array([0.0, np.radians(90.0), 0.0])
+        np.testing.assert_allclose(dtheta_out, dtheta_expect)
+        np.testing.assert_allclose(dvel_out, np.zeros(3))
+
+    def test_update_pure_yaw(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([0.0, 0.0, 0.0])  # m/s^2
+        w = np.array([0.0, 0.0, np.radians(90.0)])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out, dvel_out = alg.flush()
+
+        dtheta_expect = np.array([0.0, 0.0, np.radians(90.0)])
+        np.testing.assert_allclose(dtheta_out, dtheta_expect)
+        np.testing.assert_allclose(dvel_out, np.zeros(3))
+
+    def test_pure_surge(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([1.0, 0.0, 0.0])  # m/s^2
+        w = np.array([0.0, 0.0, 0.0])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out, dvel_out = alg.flush()
+
+        dvel_expect = np.array([1.0, 0.0, 0.0])
+        np.testing.assert_allclose(dvel_out, dvel_expect)
+        np.testing.assert_allclose(dtheta_out, np.zeros(3))
+
+    def test_pure_sway(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([0.0, 1.0, 0.0])  # m/s^2
+        w = np.array([0.0, 0.0, 0.0])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out, dvel_out = alg.flush()
+
+        dvel_expect = np.array([0.0, 1.0, 0.0])
+        np.testing.assert_allclose(dvel_out, dvel_expect)
+        np.testing.assert_allclose(dtheta_out, np.zeros(3))
+
+    def test_pure_heave(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([0.0, 0.0, 1.0])  # m/s^2
+        w = np.array([0.0, 0.0, 0.0])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out, dvel_out = alg.flush()
+
+        dvel_expect = np.array([0.0, 0.0, 1.0])
+        np.testing.assert_allclose(dvel_out, dvel_expect)
+        np.testing.assert_allclose(dtheta_out, np.zeros(3))
+
+    def test_roll_and_surge(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([1.0, 0.0, 0.0])  # m/s^2
+        w = np.array([np.radians(90.0), 0.0, 0.0])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out, dvel_out = alg.flush()
+
+        dtheta_expect = np.array([np.radians(90.0), 0.0, 0.0])
+        dvel_expect = np.array([1.0, 0.0, 0.0])
+        np.testing.assert_allclose(dtheta_out, dtheta_expect, atol=1e-8)
+        np.testing.assert_allclose(dvel_out, dvel_expect, atol=1e-8)
+
+    def test_pitch_and_sway(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([0.0, 1.0, 0.0])  # m/s^2
+        w = np.array([0.0, np.radians(90.0), 0.0])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out, dvel_out = alg.flush()
+
+        dtheta_expect = np.array([0.0, np.radians(90.0), 0.0])
+        dvel_expect = np.array([0.0, 1.0, 0.0])
+        np.testing.assert_allclose(dtheta_out, dtheta_expect, atol=1e-8)
+        np.testing.assert_allclose(dvel_out, dvel_expect, atol=1e-8)
+
+    def test_yaw_and_heave(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([0.0, 0.0, 1.0])  # m/s^2
+        w = np.array([0.0, 0.0, np.radians(90.0)])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out, dvel_out = alg.flush()
+
+        dtheta_expect = np.array([0.0, 0.0, np.radians(90.0)])
+        dvel_expect = np.array([0.0, 0.0, 1.0])
+        np.testing.assert_allclose(dtheta_out, dtheta_expect, atol=1e-8)
+        np.testing.assert_allclose(dvel_out, dvel_expect, atol=1e-8)
+
+    def test_roll_pitch_yaw(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([0.0, 0.0, 0.0])  # m/s^2
+        w = np.array([np.radians(30.0), -np.radians(45.0), np.radians(60.0)])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out, dvel_out = alg.flush()
+
+        dtheta_expect = np.array(
+            [np.radians(30.0), -np.radians(45.0), np.radians(60.0)]
+        )
+        np.testing.assert_allclose(dtheta_out, dtheta_expect, atol=1e-8)
+        np.testing.assert_allclose(dvel_out, np.zeros(3))
+
+    def test_surge_sway_heave(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([1.0, -2.0, 3.0])  # m/s^2
+        w = np.array([0.0, 0.0, 0.0])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out, dvel_out = alg.flush()
+
+        dvel_expect = np.array([1.0, -2.0, 3.0])
+        np.testing.assert_allclose(dvel_out, dvel_expect)
+        np.testing.assert_allclose(dtheta_out, np.zeros(3))
+
+    def test_dvel(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([1.0, 2.0, 3.0])  # m/s^2
+        w = np.array([0.0, 0.0, 0.0])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dvel_out = alg.dvel()
+
+        dvel_expect = np.array([1.0, 2.0, 3.0])
+        np.testing.assert_allclose(dvel_out, dvel_expect)
+
+    def test_dtheta(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([0.0, 0.0, 0.0])  # m/s^2
+        w = np.array([np.radians(30.0), -np.radians(45.0), np.radians(60.0)])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out = alg.dtheta()
+
+        dtheta_expect = np.array(
+            [np.radians(30.0), -np.radians(45.0), np.radians(60.0)]
+        )
+        np.testing.assert_allclose(dtheta_out, dtheta_expect)
+
+    def test_flush(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([1.0, 2.0, 3.0])  # m/s^2
+        w = np.array([np.radians(30.0), -np.radians(45.0), np.radians(60.0)])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_expect = alg.dtheta()
+        dvel_expect = alg.dvel()
+        dtheta_out, dvel_out = alg.flush()
+        np.testing.assert_allclose(dtheta_out, dtheta_expect)
+        np.testing.assert_allclose(dvel_out, dvel_expect)
+        assert np.allclose(alg.dtheta(), np.zeros(3))
+        assert np.allclose(alg.dvel(), np.zeros(3))
