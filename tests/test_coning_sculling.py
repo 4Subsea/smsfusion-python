b02b514c3b87e2ac5905e75c2d58a37a712047cd
Vegard R. Solum <vegard.solum@4subsea.com>
2025-12-18 13:53:56 +0100

    test dtheta

--- tests/test_coning_sculling.py
+++ tests/test_coning_sculling.py
@@ -283,3 +283,35 @@ class Test_ConingScullingAlg:
         dvel_expect = np.array([1.0, -2.0, 3.0])
         np.testing.assert_allclose(dvel_out, dvel_expect)
         np.testing.assert_allclose(dtheta_out, np.zeros(3))
+
+    def test_dvel(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([1.0, 2.0, 3.0])  # m/s^2
+        w = np.array([0.0, 0.0, 0.0])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dvel_out = alg.dvel()
+
+        dvel_expect = np.array([1.0, 2.0, 3.0])
+        np.testing.assert_allclose(dvel_out, dvel_expect)
+
+    def test_dtheta(self):
+        fs = 100.0
+        alg = sf.ConingScullingAlg(fs)
+
+        f = np.array([0.0, 0.0, 0.0])  # m/s^2
+        w = np.array([np.radians(30.0), -np.radians(45.0), np.radians(60.0)])  # rad/s
+
+        for i in range(int(fs * 1.0)):  # 1 second
+            alg.update(f, w)
+
+        dtheta_out = alg.dtheta()
+
+        dtheta_expect = np.array(
+            [np.radians(30.0), -np.radians(45.0), np.radians(60.0)]
+        )
+        np.testing.assert_allclose(dtheta_out, dtheta_expect)
