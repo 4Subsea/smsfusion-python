2182935d90b6fecfbf74b9012bf4d845b60a8420
Vegard R. Solum <vegard.solum@4subsea.com>
2025-12-19 11:43:22 +0100

    test linear ramp

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -1,8 +1,13 @@
 import numpy as np
 import pytest
 
-from smsfusion.simulate import ConstantDOF, SineDOF
+from smsfusion.simulate import ConstantDOF, SineDOF, LinearRampUp
 from smsfusion.simulate._simulate import DOF
+
+
+@pytest.fixture
+def t():
+    return np.linspace(0, 10, 100)
 
 
 class Test_DOF:
@@ -21,23 +26,19 @@ class Test_DOF:
 
         return SomeDOF()
 
-    def test_y(self, some_dof):
-        t = np.linspace(0, 10, 100)
+    def test_y(self, some_dof, t):
         y = some_dof.y(t)
         np.testing.assert_allclose(y, np.ones(100))
 
-    def test_dydt(self, some_dof):
-        t = np.linspace(0, 10, 100)
+    def test_dydt(self, some_dof, t):
         dydt = some_dof.dydt(t)
         np.testing.assert_allclose(dydt, 2 * np.ones(100))
 
-    def test_d2ydt2(self, some_dof):
-        t = np.linspace(0, 10, 100)
+    def test_d2ydt2(self, some_dof, t):
         d2ydt2 = some_dof.d2ydt2(t)
         np.testing.assert_allclose(d2ydt2, 3 * np.ones(100))
 
-    def test__call__(self, some_dof):
-        t = np.linspace(0, 10, 100)
+    def test__call__(self, some_dof, t):
         y, dydt, dy2dt2 = some_dof(t)
         np.testing.assert_allclose(y, np.ones(100))
         np.testing.assert_allclose(dydt, 2 * np.ones(100))
@@ -54,23 +55,19 @@ class Test_ConstantDOF:
         assert isinstance(constant_dof, DOF)
         assert constant_dof._value == 123.0
 
-    def test_y(self, constant_dof):
-        t = np.linspace(0, 10, 100)
+    def test_y(self, constant_dof, t):
         y = constant_dof.y(t)
         np.testing.assert_allclose(y, 5.0 * np.ones(100))
 
-    def test_dydt(self, constant_dof):
-        t = np.linspace(0, 10, 100)
+    def test_dydt(self, constant_dof, t):
         dydt = constant_dof.dydt(t)
         np.testing.assert_allclose(dydt, np.zeros(100))
 
-    def test_d2ydt2(self, constant_dof):
-        t = np.linspace(0, 10, 100)
+    def test_d2ydt2(self, constant_dof, t):
         d2ydt2 = constant_dof.d2ydt2(t)
         np.testing.assert_allclose(d2ydt2, np.zeros(100))
 
-    def test__call__(self, constant_dof):
-        t = np.linspace(0, 10, 100)
+    def test__call__(self, constant_dof, t):
         y, dydt, dy2dt2 = constant_dof(t)
         np.testing.assert_allclose(y, 5.0 * np.ones(100))
         np.testing.assert_allclose(dydt, np.zeros(100))
@@ -81,10 +78,6 @@ class Test_SineDOF:
     @pytest.fixture
     def sine_dof(self):
         return SineDOF(2.0, 1.0)
-
-    @pytest.fixture
-    def t(self):
-        return np.linspace(0, 10, 100)
 
     def test__init__(self):
         sine_dof = SineDOF(
@@ -181,3 +174,53 @@ class Test_SineDOF:
         np.testing.assert_allclose(y, y_expect)
         np.testing.assert_allclose(dydt, dydt_expect)
         np.testing.assert_allclose(dy2dt2, dy2dt2_expect)
+
+
+class Test_LinearRampUp:
+    @pytest.fixture
+    def dof(self):
+        return SineDOF(amp=1.0, freq=1.0, freq_hz=True)
+
+    @pytest.fixture
+    def dof_with_ramp(self, dof):
+        return LinearRampUp(dof, t_start=1.0, ramp_length=2.0)
+
+    def test__init__(self, dof):
+        dof_ramp = LinearRampUp(dof, t_start=0.5, ramp_length=3.0)
+        assert isinstance(dof_ramp, DOF)
+        assert dof_ramp._dof is dof
+        assert dof_ramp._t_start == 0.5
+        assert dof_ramp._ramp_length == 3.0
+
+    def test_y(self, dof_with_ramp, dof, t):
+        y = dof_with_ramp.y(t)
+
+        before_ramp = t < 1.0
+        during_ramp = (t >= 1.0) & (t < 3.0)
+        after_ramp = t >= 3.0
+
+        y_dof, dydt_dof, d2ydt2_dof = dof(t)
+        y_expect = np.where(
+            before_ramp, 0.0, np.where(during_ramp, (t - 1.0) / 2.0 * y_dof, y_dof)
+        )
+        dydt_expect = np.where(
+            before_ramp,
+            0.0,
+            np.where(
+                during_ramp,
+                ((t - 1.0) / 2.0) * dydt_dof,
+                dydt_dof,
+            ),
+        )
+        d2ydt2_expect = np.where(
+            before_ramp,
+            0.0,
+            np.where(
+                during_ramp,
+                ((t - 1.0) / 2.0) * d2ydt2_dof,
+                d2ydt2_dof,
+            ),
+        )
+        np.testing.assert_allclose(y, y_expect)
+        np.testing.assert_allclose(dof_with_ramp.dydt(t), dydt_expect)
+        np.testing.assert_allclose(dof_with_ramp.d2ydt2(t), d2ydt2_expect)
