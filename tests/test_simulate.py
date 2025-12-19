0777fcc831593720dd44006935709ed2ee747c74
Vegard R. Solum <vegard.solum@4subsea.com>
2025-12-19 10:56:53 +0100

    test DOF

---
+++ tests/test_simulate.py
@@ -0,0 +1,43 @@
+import pytest
+import numpy as np
+
+from smsfusion.simulate._simulate import DOF
+
+
+class Test_DOF:
+    @pytest.fixture
+    def some_dof(self):
+        class SomeDOF(DOF):
+
+            def _y(self, t):
+                return np.ones_like(t)
+
+            def _dydt(self, t):
+                return 2 * np.ones_like(t)
+
+            def _d2ydt2(self, t):
+                return 3 *np.ones_like(t)
+            
+        return SomeDOF()
+
+    def test_y(self, some_dof):
+        t = np.linspace(0, 10, 100)
+        y = some_dof.y(t)
+        np.testing.assert_allclose(y, np.ones(100))
+
+    def test_dydt(self, some_dof):
+        t = np.linspace(0, 10, 100)
+        dydt = some_dof.dydt(t)
+        np.testing.assert_allclose(dydt, 2 * np.ones(100))
+
+    def test_d2ydt2(self, some_dof):
+        t = np.linspace(0, 10, 100)
+        d2ydt2 = some_dof.d2ydt2(t)
+        np.testing.assert_allclose(d2ydt2, 3 * np.ones(100))
+
+    def test__call__(self, some_dof):
+        t = np.linspace(0, 10, 100)
+        y, dydt, dy2dt2 = some_dof(t)
+        np.testing.assert_allclose(y, np.ones(100))
+        np.testing.assert_allclose(dydt, 2 * np.ones(100))
+        np.testing.assert_allclose(dy2dt2, 3 * np.ones(100))
