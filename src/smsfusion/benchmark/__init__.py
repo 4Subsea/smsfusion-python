ec32ae6cef02dbf5c763f96517b1daa487db1766
Vegard R. Solum <vegard.solum@4subsea.com>
2025-12-22 09:14:47 +0100

    delete obsolete benchmark helper

--- src/smsfusion/benchmark/__init__.py
+++ src/smsfusion/benchmark/__init__.py
@@ -1,7 +1,6 @@
 from ._benchmark import (
     BeatSignal,
     ChirpSignal,
-    _benchmark_helper,
     benchmark_full_pva_beat_202311A,
     benchmark_full_pva_chirp_202311A,
     benchmark_pure_attitude_beat_202311A,
@@ -9,7 +8,6 @@ from ._benchmark import (
 )
 
 __all__ = [
-    "_benchmark_helper",
     "BeatSignal",
     "benchmark_full_pva_beat_202311A",
     "benchmark_full_pva_chirp_202311A",
