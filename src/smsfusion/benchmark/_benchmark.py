a17c34656f539c9432454339adfb688f9fb7f2dc
Vegard R. Solum <vegard.solum@4subsea.com>
2025-12-22 09:46:18 +0100

    delete obsolete imports

--- src/smsfusion/benchmark/_benchmark.py
+++ src/smsfusion/benchmark/_benchmark.py
@@ -6,8 +6,6 @@ from warnings import warn
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
 
-from smsfusion._ins import gravity
-from smsfusion._transforms import _inv_angular_matrix_from_euler, _rot_matrix_from_euler
 from smsfusion.simulate import BeatDOF, ChirpDOF, IMUSimulator
 
 
