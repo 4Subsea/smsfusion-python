1cf779f6d77d175cfc5514d97d3f45dc794f8813
Vegard R. Solum <vegard.solum@4subsea.com>
2026-03-13 11:42:42 +0100

    init

--- src/smsfusion/__init__.py
+++ src/smsfusion/__init__.py
@@ -3,13 +3,14 @@ from ._coning_sculling import ConingScullingAlg
 from ._ins import AHRS, VRU, AidedINS, FixedNED, StrapdownINS, gravity
 from ._smoothing import FixedIntervalSmoother
 from ._transforms import quaternion_from_euler
-from ._v2 import AHRSv2a, AHRSv2b, AHRSv2c
+from ._v2 import AHRSv2a, AHRSv2b, AHRSv2c, AHRSv2d
 
 __all__ = [
     "AHRS",
     "AHRSv2a",
     "AHRSv2b",
     "AHRSv2c",
+    "AHRSv2d",
     "AidedINS",
     "benchmark",
     "constants",
