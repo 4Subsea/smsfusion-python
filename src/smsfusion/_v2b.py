49d98f715a85b88b9913372a91a18e5bdbeca394
Vegard R. Solum <vegard.solum@4subsea.com>
2026-03-13 08:54:57 +0100

    reorder vel first

--- src/smsfusion/_v2b.py
+++ src/smsfusion/_v2b.py
@@ -10,6 +10,12 @@ from ._transforms import _euler_from_quaternion
 from ._vectorops import _normalize, _skew_symmetric
 
 
+ATT_IDX = slice(0, 3)
+BG_IDX = slice(3, 6)
+VEL_IDX = slice(6, 9)
+
+
+
 def _state_transition(
     dt: float, f_b: NDArray[np.float64], w_b: NDArray[np.float64], R_nb: NDArray[np.float64], gbc: float
 ) -> NDArray[np.float64]:
@@ -35,10 +41,10 @@ def _state_transition(
         State transition matrix.
     """
     phi = np.eye(9)
-    phi[0:3, 0:3] -= dt * _skew_symmetric(w_b)  # NB! update each time step
-    phi[0:3, 3:6] -= dt * np.eye(3)
-    phi[3:6, 3:6] -= dt * np.eye(3) / gbc
-    phi[6:9, 0:3] -= dt * R_nb @ _skew_symmetric(f_b)  # NB! update each time step
+    phi[VEL_IDX, ATT_IDX] -= dt * R_nb @ _skew_symmetric(f_b)  # NB! update each time step
+    phi[ATT_IDX, ATT_IDX] -= dt * _skew_symmetric(w_b)  # NB! update each time step
+    phi[ATT_IDX, BG_IDX] -= dt * np.eye(3)
+    phi[BG_IDX, BG_IDX] -= dt * np.eye(3) / gbc
     return phi
 
 
@@ -73,28 +79,28 @@ def _update_state_transition(
     r10, r11, r12 = R_nb[1]
     r20, r21, r22 = R_nb[2]
 
-    # phi[0:3, 0:3] = np.eye(3) - dt * S(w_b)
-    phi[0, 1] = dt * wz
-    phi[0, 2] = -dt * wy
-    phi[1, 0] = -dt * wz
-    phi[1, 2] = dt * wx
-    phi[2, 0] = dt * wy
-    phi[2, 1] = -dt * wx
+    # phi[3:6, 3:6] = np.eye(3) - dt * S(w_b)
+    phi[3, 4] = dt * wz
+    phi[3, 5] = -dt * wy
+    phi[4, 3] = -dt * wz
+    phi[4, 5] = dt * wx
+    phi[5, 3] = dt * wy
+    phi[5, 4] = -dt * wx
 
-    # phi[6:9, 0:3] = -dt * R_nb @ S(f_b)
-    phi[6, 0] = -dt * (fz * r01 - fy * r02)
-    phi[7, 0] = -dt * (fz * r11 - fy * r12)
-    phi[8, 0] = -dt * (fz * r21 - fy * r22)
-    phi[6, 1] = -dt * (-fz * r00 + fx * r02)
-    phi[7, 1] = -dt * (-fz * r10 + fx * r12)
-    phi[8, 1] = -dt * (-fz * r20 + fx * r22)
-    phi[6, 2] = -dt * (fy * r00 - fx * r01)
-    phi[7, 2] = -dt * (fy * r10 - fx * r11)
-    phi[8, 2] = -dt * (fy * r20 - fx * r21)
+    # phi[0:3, 3:6] = -dt * R_nb @ S(f_b)
+    phi[0, 3] = -dt * (fz * r01 - fy * r02)
+    phi[1, 3] = -dt * (fz * r11 - fy * r12)
+    phi[2, 3] = -dt * (fz * r21 - fy * r22)
+    phi[0, 4] = -dt * (-fz * r00 + fx * r02)
+    phi[1, 4] = -dt * (-fz * r10 + fx * r12)
+    phi[2, 4] = -dt * (-fz * r20 + fx * r22)
+    phi[0, 5] = -dt * (fy * r00 - fx * r01)
+    phi[1, 5] = -dt * (fy * r10 - fx * r11)
+    phi[2, 5] = -dt * (fy * r20 - fx * r21)
 
 
 def _process_noise_cov(
-    dt: float, arw: float, gbs: float, gbc: float
+    dt: float, vrw: float, arw: float, gbs: float, gbc: float
 ) -> NDArray[np.float64]:
     """
     Process noise covariance matrix.
@@ -103,6 +109,8 @@ def _process_noise_cov(
     ----------
     dt : float
         Time step in seconds.
+    vrw : float
+        Velocity random walk (accelerometer noise density) in m/s/√Hz.
     arw : float
         Angular random walk (gyroscope noise density) in rad/√Hz.
     gbs : float
@@ -112,12 +120,13 @@ def _process_noise_cov(
 
     Returns
     -------
-    Q : ndarray, shape (6, 6)
+    Q : ndarray, shape (9, 9)
         Process noise covariance matrix.
     """
-    Q = np.zeros((6, 6))
-    Q[0:3, 0:3] = dt * arw**2 * np.eye(3)
-    Q[3:6, 3:6] = dt * (2.0 * gbs**2 / gbc) * np.eye(3)
+    Q = np.zeros((9, 9))
+    Q[VEL_IDX, VEL_IDX] = dt * vrw**2 * np.eye(3)
+    Q[ATT_IDX, ATT_IDX] = dt * arw**2 * np.eye(3)
+    Q[BG_IDX, BG_IDX] = dt * (2.0 * gbs**2 / gbc) * np.eye(3)
     return Q
 
 
@@ -139,8 +148,8 @@ def _measurement_matrix(
     ndarray, shape (4, 6)
         Linearized measurement matrix.
     """
-    dhdx = np.zeros((4, 6))
-    dhdx[0:3, 0:3] = _skew_symmetric(vg_b)  # gravity ref vector
-    dhdx[3:4, 0:3] = _dhda_head(q_nb)  # heading
+    dhdx = np.zeros((4, 9))
+    dhdx[0:3, ATT_IDX] = _skew_symmetric(vg_b)  # gravity ref vector
+    dhdx[3:4, ATT_IDX] = _dhda_head(q_nb)  # heading
     return dhdx
 
