2dc3856cac6ca355621cf4b6219cfccec457ab8f
Vegard R. Solum <vegard.solum@4subsea.com>
2026-03-14 08:50:31 +0100

    style

--- src/smsfusion/_v2.py
+++ src/smsfusion/_v2.py
@@ -14,7 +14,9 @@ BG_IDX = slice(6, 9)
 
 
 @njit  # type: ignore[misc]
-def _correct_quaternion_with_gibbs2(q: NDArray[np.float64], da: NDArray[np.float64]) -> None:
+def _correct_quaternion_with_gibbs2(
+    q: NDArray[np.float64], da: NDArray[np.float64]
+) -> None:
     """
     Corrects a unit quaternion, q, with a small attitude error, da, parameterized
     as a scaled (2x) Gibbs vector.
