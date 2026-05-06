d45788e8984fa1504bebdc0732baeafc8d4a5c26
Per Sigmond <psi@4subsea.com>
2026-05-06 13:17:05 +0200

    Changed init for vru_v2

--- src/smsfusion/__init__.py
+++ src/smsfusion/__init__.py
@@ -1,4 +1,4 @@
-from ._ins_v2 import AHRSv2 # hidden
+from ._vru_v2 import VRUv2 # hidden
 from . import benchmark, calibrate, constants, noise
 from ._coning_sculling import ConingScullingAlg
 from ._ins import AHRS, VRU, AidedINS, FixedNED, StrapdownINS, gravity
@@ -6,7 +6,7 @@ from ._smoothing import FixedIntervalSmoother
 from ._transforms import quaternion_from_euler
 
 __all__ = [
-    "AHRSv2",
+    "VRUv2",
     "AidedINS",
     "benchmark",
     "constants",
