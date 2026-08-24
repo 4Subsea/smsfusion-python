fb800efd6d88426a8079e97d8b75c6f91c74571c
Vegard R. Solum <vegard.solum@4subsea.com>
2026-08-24 08:22:02 +0200

    add ains v2 to init

--- src/smsfusion/_ins/__init__.py
+++ src/smsfusion/_ins/__init__.py
@@ -2,3 +2,4 @@ from ._ahrs import AHRS as AHRSv2
 from ._ains import AHRS, VRU, AidedINS, StrapdownINS
 from ._utils import FixedNED, gravity, euler_from_acc
 from ._vru import VRU as VRUv2
+from ._ains_ import AINS as AINSv2
