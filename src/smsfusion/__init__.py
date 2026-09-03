94d3edb01169420ec383656a719c385039819949
Ali Cetin <ali.cetin@4subsea.com>
2026-09-03 14:57:11 +0200

    remove VRU, AHRS, AidedINS, Strapdown stuff

--- src/smsfusion/__init__.py
+++ src/smsfusion/__init__.py
@@ -1,31 +1,23 @@
 from . import benchmark, calibrate, constants, noise
 from ._coning_sculling import ConingScullingAlg, ConingScullingAlgCalibrated
 from ._ins import (
-    AHRS,
     AMEKF,
     PVAMEKF,
     VAMEKF,
-    VRU,
-    AidedINS,
     FixedNED,
-    StrapdownINS,
     gravity,
 )
 from ._smoothing import FixedIntervalSmoother
 from ._transforms import quaternion_from_euler
 
 __all__ = [
-    "AHRS",
     "AMEKF",
     "PVAMEKF",
     "VAMEKF",
-    "VRU",
-    "AidedINS",
     "ConingScullingAlg",
     "ConingScullingAlgCalibrated",
     "FixedIntervalSmoother",
     "FixedNED",
-    "StrapdownINS",
     "benchmark",
     "calibrate",
     "constants",
