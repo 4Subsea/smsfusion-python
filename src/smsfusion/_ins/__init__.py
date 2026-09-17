8eeeeebac610abf583916d9d06a9307acc411151
Ali Cetin <ali.cetin@4subsea.com>
2026-09-17 08:55:34 +0200

    enh: deprecate legacy filters VRU, AHRS, and AidedINS + delete legacy FixedIntervalSmoother (#104)

--- src/smsfusion/_ins/__init__.py
+++ src/smsfusion/_ins/__init__.py
@@ -1,18 +1,13 @@
-from ._ains_legacy import AHRS, VRU, AidedINS, StrapdownINS
 from ._amekf import AMEKF
 from ._pvamekf import PVAMEKF
 from ._utils import FixedNED, euler_from_acc, gravity
 from ._vamekf import VAMEKF
 
 __all__ = [
-    "AHRS",
     "AMEKF",
     "PVAMEKF",
     "VAMEKF",
-    "VRU",
-    "AidedINS",
     "FixedNED",
-    "StrapdownINS",
     "euler_from_acc",
     "gravity",
 ]
--- src/smsfusion/constants/_constants.py
+++ src/smsfusion/constants/_constants.py
@@ -1,15 +1,4 @@
-import numpy as np
-from numpy.typing import NDArray
-
-# Default initial state vector
-X0: NDArray[np.float64] = np.array(
-    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-)
-
-# Default initial covariance matrix
-P0: NDArray[np.float64] = np.eye(12) * 1e-6
-
-# Noise and bias parameters for SMS Motion Gen 2
+# Noise parameters for SMS Motion Gen 2
 ERR_ACC_MOTION2: dict[str, float] = {
     "N": 0.0007,  # (m/s^2)/sqrt(Hz)
     "B": 0.0005,  # m/s^2
